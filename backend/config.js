1ef9491ed73fd22929e95ae4e6a637970aaa973f
esmertz <119896591+esmertz@users.noreply.github.com>
2024-11-03 19:13:45 -0500

    Completed Backend
    I setup backend. Now we can do GET, POST, UPDATE, and DELETE requests for a book. I set up the mongodb cluster to make this possible.

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://Esmertz:Harps716!@ready2read.eadqy.mongodb.net/?retryWrites=true&w=majority&appName=Ready2Read'
---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express, { request, response } from "express";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js'
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling CORS Policy
+app.use(cors());
+
+app.get('/', (request, response)=>{
+    console.log(request)
+    return response.status(234).send('Ready2Read')
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() =>{
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) =>{
+        console.log(error);
+        
+    });
